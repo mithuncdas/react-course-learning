97131e338a5419e3fe12fb2556bca9c009668104
Mithun Das <mithuncse404@gmail.com>
2022-04-04 08:30:14 +0600

    component with style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <h1>My First React app</h1>
       <MyFirstComponent></MyFirstComponent>
+      <InlineStyleComponent></InlineStyleComponent>
+      <PredefinedStyleComponent></PredefinedStyleComponent>
     </div>
   );
 }
@@ -13,6 +15,33 @@ function MyFirstComponent(){
   return (
     <div>
       <h1>My FirstComponent</h1>
+    </div>
+  );
+}
+
+/**
+ * component with style inline style
+ */
+function InlineStyleComponent(){
+  return (
+    <div>
+      <h1 style={{ border:'2px solid red',background:'white',color:'red',padding:'10px' }}>Inline Style Component</h1>
+    </div>
+  );
+}
+/**
+ * component with predefined style
+ */
+function PredefinedStyleComponent(){
+  const style = {
+    border:'2px solid red',
+    background:'white',
+    color:'red',
+    padding:'10px'
+  }
+  return (
+    <div>
+      <h1 style={style}>Predefiend style Component </h1>
     </div>
   );
 }
