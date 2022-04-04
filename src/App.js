0f124732f92ace5f32d74fd53c42bea23363dc43
Mithun Das <mithuncse404@gmail.com>
2022-04-04 20:55:05 +0600

    use effect complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -27,6 +27,7 @@ function App() {
         infos.map(info => <DaynamicComponent info={info}></DaynamicComponent>)
       }
       <ReactState></ReactState>
+      <Users></Users>
     </div>
   );
 }
@@ -133,6 +134,28 @@ function ReactState(){
       <h4>Your age is :{age} </h4>
       <button onClick={ageIncrease}>Increase</button>
       <button onClick={() => setAge(age - 1)}>Decrease</button>
+    </div>
+  );
+}
+
+/**
+ * React Use Effect
+ */
+function Users(){
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  })
+  return (
+    <div>
+      <h3>Dynamic User Name:{users.length} </h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
