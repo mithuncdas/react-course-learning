86791dcc5a2010554011675e8a75fe0460bfb947
Mithun Das <mithuncse404@gmail.com>
2022-04-04 08:57:29 +0600

    simple props, using arrya, using arrray object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 
 function App() {
+  const names = ['Mithun','Rahim','karim','sokina','jorina']
+  const infos = [
+    {name:'Mithun',age:'10'},
+    {name:'Rahim',age:'20'},
+    {name:'Karim',age:'40'},
+    {name:'Sokina',age:'60'},
+    {name:'Jorina',age:'70'},
+  ]
   return (
     <div className="App">
       <h1>My First React app</h1>
       <MyFirstComponent></MyFirstComponent>
       <InlineStyleComponent></InlineStyleComponent>
       <PredefinedStyleComponent></PredefinedStyleComponent>
+      <MyFirstProps name="Mithun Das"></MyFirstProps>
+      <MyArrayProps name={names[0]}></MyArrayProps>
+      <MyArrayProps name={names[1]}></MyArrayProps>
+      <MyArrayProps name={names[2]}></MyArrayProps>
+      <MyArrayProps name={names[3]}></MyArrayProps>
+      <MyArrayWithObjectProps info={infos[0]}></MyArrayWithObjectProps>
+      <MyArrayWithObjectProps info={infos[1]}></MyArrayWithObjectProps>
     </div>
   );
 }
@@ -42,6 +57,47 @@ function PredefinedStyleComponent(){
   return (
     <div>
       <h1 style={style}>Predefiend style Component </h1>
+    </div>
+  );
+}
+
+/**
+ * react simple props
+ */
+function MyFirstProps(props){
+  const divStyle={
+    border:'2px solid red',
+    background:'whitegray',
+  }
+  return (
+    <div style={divStyle}>
+      <h1 style={{ color:'red' }}>My First Props</h1>
+      <h3>My name is {props.name}</h3>
+    </div>
+  );
+}
+
+/**
+ * react props using array
+ */
+function MyArrayProps(props){
+  return (
+    <div>
+      <h1>Props using array</h1>
+      <h3> My name is {props.name}</h3>
+    </div>
+  );
+}
+
+/**
+ * react props using 
+ */
+function MyArrayWithObjectProps(props){
+  return (
+    <div>
+      <h1>Props using array object</h1>
+      <h3>My name is {props.info.name}</h3>
+      <h4>My age is {props.info.age}</h4>
     </div>
   );
 }
