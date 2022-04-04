afbda3e608dfd4700c5340611982d93e4a225df0
Mithun Das <mithuncse404@gmail.com>
2022-04-04 08:13:27 +0600

    first component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>My First React app</h1>
+      <MyFirstComponent></MyFirstComponent>
+    </div>
+  );
+}
+
+function MyFirstComponent(){
+  return (
+    <div>
+      <h1>My FirstComponent</h1>
     </div>
   );
 }
